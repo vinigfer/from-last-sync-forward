ad0df0022372e27bd18dd25ae2f224fa3e1e4650
Vinicius Ferreira <vini.g.fer@gmail.com>
2016-07-26 10:49:25 -0300

    - Fixed Ctrl+C issue to interrupt - Added flv and rar files

--- script.sh
+++ script.sh
@@ -13,8 +13,9 @@ ssh $user@$server '(find /home/'"$user"'/Transmission_Downloads/ \
                                                -iname "*.mkv" -o \
                                                -iname "*.avi" -o \
                                                -iname "*.zip" -o \
-                                               -iname "*.pdf" \) \
-                                    -newermt '\""$Last_time_checked"\"')' > $File_list
+                                               -iname "*.flv" -o \
+                                               -iname "*.rar" \) \
+                                    -newermt '\""$Last_time_checked"\"')' | grep -v sample | | sort -r > $File_list
 
 echo "The following files will be downloaded:"
 cat $File_list
@@ -22,6 +23,12 @@ cat $File_list
 cat $File_list | while read File_path_on_server
 do
     rsync --protect-args -aP "$user@$server:$File_path_on_server" $local_folder
+    #Capture rsync error if we hit Ctrl+C . Prevent date file from being updated
+    if [ "$?" -ne "0" ]
+    then
+        sleep 2
+        kill 0
+    fi
 done
 
 rm $File_list
